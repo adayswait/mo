16cfa3f9ab5badbb0de8f18bab37a025ff22e3db
adayswait <adayswait@139.com>
2019-12-23 14:52:46 +0800

    build on windows

--- inc/me.h
+++ inc/me.h
@@ -1,5 +1,5 @@
 /*
-    me.h -- MakeMe Configure Header for linux-x86-static
+    me.h -- MakeMe Configure Header for windows-x64-static
 
     This header is created by Me during configuration. To change settings, re-run
     configure or define variables in your Makefile to override these default values.
@@ -33,9 +33,6 @@
 #ifndef ME_COMPILER_HAS_ATOMIC64
     #define ME_COMPILER_HAS_ATOMIC64 0
 #endif
-#ifndef ME_COMPILER_HAS_DOUBLE_BRACES
-    #define ME_COMPILER_HAS_DOUBLE_BRACES 0
-#endif
 #ifndef ME_COMPILER_HAS_DYN_LOAD
     #define ME_COMPILER_HAS_DYN_LOAD 1
 #endif
@@ -43,37 +40,25 @@
     #define ME_COMPILER_HAS_LIB_EDIT 0
 #endif
 #ifndef ME_COMPILER_HAS_LIB_RT
-    #define ME_COMPILER_HAS_LIB_RT 1
+    #define ME_COMPILER_HAS_LIB_RT 0
 #endif
 #ifndef ME_COMPILER_HAS_MMU
     #define ME_COMPILER_HAS_MMU 1
 #endif
-#ifndef ME_COMPILER_HAS_MTUNE
-    #define ME_COMPILER_HAS_MTUNE 1
-#endif
-#ifndef ME_COMPILER_HAS_PAM
-    #define ME_COMPILER_HAS_PAM 0
-#endif
 #ifndef ME_COMPILER_HAS_STACK_PROTECTOR
-    #define ME_COMPILER_HAS_STACK_PROTECTOR 1
+    #define ME_COMPILER_HAS_STACK_PROTECTOR 0
 #endif
 #ifndef ME_COMPILER_HAS_SYNC
-    #define ME_COMPILER_HAS_SYNC 1
+    #define ME_COMPILER_HAS_SYNC 0
 #endif
 #ifndef ME_COMPILER_HAS_SYNC64
-    #define ME_COMPILER_HAS_SYNC64 1
+    #define ME_COMPILER_HAS_SYNC64 0
 #endif
 #ifndef ME_COMPILER_HAS_SYNC_CAS
     #define ME_COMPILER_HAS_SYNC_CAS 0
 #endif
 #ifndef ME_COMPILER_HAS_UNNAMED_UNIONS
     #define ME_COMPILER_HAS_UNNAMED_UNIONS 1
-#endif
-#ifndef ME_COMPILER_WARN64TO32
-    #define ME_COMPILER_WARN64TO32 0
-#endif
-#ifndef ME_COMPILER_WARN_UNUSED
-    #define ME_COMPILER_WARN_UNUSED 1
 #endif
 #ifndef ME_DEBUG
     #define ME_DEBUG 1
@@ -132,77 +117,80 @@
 
 /* Prefixes */
 #ifndef ME_ROOT_PREFIX
-    #define ME_ROOT_PREFIX "/"
+    #define ME_ROOT_PREFIX "C:"
+#endif
+#ifndef ME_PROGRAMFILES_PREFIX
+    #define ME_PROGRAMFILES_PREFIX "C:/Program Files"
+#endif
+#ifndef ME_PROGRAMFILES32_PREFIX
+    #define ME_PROGRAMFILES32_PREFIX "C:/Program Files"
 #endif
 #ifndef ME_BASE_PREFIX
-    #define ME_BASE_PREFIX "/usr/local"
-#endif
-#ifndef ME_DATA_PREFIX
-    #define ME_DATA_PREFIX "/"
-#endif
-#ifndef ME_STATE_PREFIX
-    #define ME_STATE_PREFIX "/var"
+    #define ME_BASE_PREFIX "C:/Program Files"
 #endif
 #ifndef ME_APP_PREFIX
-    #define ME_APP_PREFIX "/usr/local/lib/http"
+    #define ME_APP_PREFIX "C:/Program Files/Embedthis Http"
 #endif
 #ifndef ME_VAPP_PREFIX
-    #define ME_VAPP_PREFIX "/usr/local/lib/http/8.0.3"
+    #define ME_VAPP_PREFIX "C:/Program Files/Embedthis Http"
+#endif
+#ifndef ME_DATA_PREFIX
+    #define ME_DATA_PREFIX "C:/Program Files/Embedthis Http"
+#endif
+#ifndef ME_STATE_PREFIX
+    #define ME_STATE_PREFIX "C:/Program Files/Embedthis Http"
 #endif
 #ifndef ME_BIN_PREFIX
-    #define ME_BIN_PREFIX "/usr/local/bin"
+    #define ME_BIN_PREFIX "C:/Program Files/Embedthis Http/bin"
 #endif
 #ifndef ME_INC_PREFIX
-    #define ME_INC_PREFIX "/usr/local/include"
+    #define ME_INC_PREFIX "C:/Program Files/Embedthis Http/inc"
 #endif
 #ifndef ME_LIB_PREFIX
-    #define ME_LIB_PREFIX "/usr/local/lib"
+    #define ME_LIB_PREFIX "C:/Program Files/Embedthis Http/lib"
 #endif
 #ifndef ME_MAN_PREFIX
-    #define ME_MAN_PREFIX "/usr/local/share/man"
-#endif
-#ifndef ME_SBIN_PREFIX
-    #define ME_SBIN_PREFIX "/usr/local/sbin"
+    #define ME_MAN_PREFIX "C:/Program Files/Embedthis Http/man"
 #endif
 #ifndef ME_ETC_PREFIX
-    #define ME_ETC_PREFIX "/etc/http"
+    #define ME_ETC_PREFIX "C:/Program Files/Embedthis Http"
 #endif
 #ifndef ME_WEB_PREFIX
-    #define ME_WEB_PREFIX "/var/www/http"
+    #define ME_WEB_PREFIX "C:/Program Files/Embedthis Http/web"
 #endif
 #ifndef ME_LOG_PREFIX
-    #define ME_LOG_PREFIX "/var/log/http"
+    #define ME_LOG_PREFIX "C:/Program Files/Embedthis Http/log"
 #endif
 #ifndef ME_SPOOL_PREFIX
-    #define ME_SPOOL_PREFIX "/var/spool/http"
+    #define ME_SPOOL_PREFIX "C:/Program Files/Embedthis Http/tmp"
 #endif
 #ifndef ME_CACHE_PREFIX
-    #define ME_CACHE_PREFIX "/var/spool/http/cache"
+    #define ME_CACHE_PREFIX "C:/Program Files/Embedthis Http/cache"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "http-8.0.3"
+    #define ME_SRC_PREFIX "C:/Program Files/Embedthis Http/src"
 #endif
 
 /* Suffixes */
 #ifndef ME_EXE
-    #define ME_EXE ""
+    #define ME_EXE ".exe"
 #endif
 #ifndef ME_SHLIB
-    #define ME_SHLIB ".so"
+    #define ME_SHLIB ".lib"
 #endif
 #ifndef ME_SHOBJ
-    #define ME_SHOBJ ".so"
+    #define ME_SHOBJ ".dll"
 #endif
 #ifndef ME_LIB
-    #define ME_LIB ".a"
+    #define ME_LIB ".lib"
 #endif
 #ifndef ME_OBJ
-    #define ME_OBJ ".o"
+    #define ME_OBJ ".obj"
 #endif
 
 /* Profile */
 #ifndef ME_CONFIG_CMD
-    #define ME_CONFIG_CMD "me -d -q -platform linux-x86-static -static -configure . -gen make"
+    #define ME_CONFIG_CMD "me -d -q -platform windows-x64-static -static -configure . -gen vs"
 #endif
 #ifndef ME_HTTP_PRODUCT
     #define ME_HTTP_PRODUCT 1
@@ -235,6 +223,9 @@
 #ifndef ME_COM_LIB
     #define ME_COM_LIB 1
 #endif
+#ifndef ME_COM_LINK
+    #define ME_COM_LINK 1
+#endif
 #ifndef ME_COM_MATRIXSSL
     #define ME_COM_MATRIXSSL 0
 #endif
@@ -255,6 +246,9 @@
 #endif
 #ifndef ME_COM_PCRE
     #define ME_COM_PCRE 1
+#endif
+#ifndef ME_COM_RC
+    #define ME_COM_RC 1
 #endif
 #ifndef ME_COM_SSL
     #define ME_COM_SSL 1
